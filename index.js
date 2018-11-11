a8ebd94ea72d53aaa44fce841d9534ce93608429
KyoshinSan <34346536+KyoshinSan@users.noreply.github.com>
2018-11-11 11:32:35 +0100

    Upload random answer

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const inquirer = require('inquirer')
 let quizz = {}
 
 
-axios.get("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium")
+axios.get("https://opentdb.com/api.php?amount=10&difficulty=medium")
 	.then(response =>  quizz = response.data.results[0])
 	.then(() => input(quizz.question, quizz.correct_answer, quizz.incorrect_answers))
 	.catch(err => console.log("err"))
@@ -14,23 +14,22 @@ axios.get("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium")
 function input(question, correct_answer, incorrect_answers) {
 let la_reponse_bonne = correct_answer
 let les_reponse_mauvaise = incorrect_answers
+let random = Math.floor(Math.random() * (4))
+let choice_answer = incorrect_answers
+choice_answer.splice(random, 0, correct_answer);
 inquirer.prompt(
 {
 	type: 'list',
 	message: question,
 	name: 'reponse',
-	choices: [
-	correct_answer,
-	incorrect_answers[0],
-	incorrect_answers[1],
-	incorrect_answers[2]
-	]
+	choices: choice_answer
 }).then((answers) => {
 	answers.reponse
 	if(answers.reponse == la_reponse_bonne) {
 		console.log('Bien joué')
 	} else {
 		console.log('Té nul !')
+		console.log(la_reponse_bonne)
 	}
 })	
 }
